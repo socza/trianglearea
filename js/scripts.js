850babc8c39f1b678fc73ed2815d8a422dc33c44
Piotr Sociński <psocinski@gmail.com>
2017-12-24 00:03:06 +0100

    The task is finished

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+var a = 0,
+	h = 0,
+	triangleArea = 0;
+
+a = prompt('Wstaw podstawę trójkąta a:');
+h = prompt('Wstaw wysokość trójkąta h:');
+
+triangleArea = a*h/2;
+alert('Pole trójkąta z podstawą a: ' + a + ' i wysokością h: ' + h + ' jest równe: ' + triangleArea);
+console.log('Pole trójkąta z podstawą a: ' + a + ' i wysokością h: ' + h + ' jest równe: ' + triangleArea);
